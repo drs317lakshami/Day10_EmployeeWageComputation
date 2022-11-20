a851cbe05bf5395855d1023109e7f737c2c89f60
drs317lakshami <devrajkumar317@gmail.com>
2022-11-20 14:04:26 +0530

    Added : created class method

---
+++ Employee.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day10;
+
+
+public class EmpWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		Employee emp = new Employee();
+		String str = emp.employeeAttendance();
+		System.out.println(str);
+	}
+}
